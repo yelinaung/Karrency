0b14880be453821b01d47bc405412c7f38aed2a2
Ye Lin Aung <me@yelinaung.com>
2014-04-28 21:56:53 +0630

    Widget is (sort of) working now.

--- app/src/main/java/com/yelinaung/karrency/app/ui/widget/ConfigureCurrency.java
+++ app/src/main/java/com/yelinaung/karrency/app/ui/widget/ConfigureCurrency.java
@@ -17,11 +17,11 @@
 package com.yelinaung.karrency.app.ui.widget;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
@@ -29,6 +29,7 @@ import android.widget.RemoteViews;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import com.yelinaung.karrency.app.R;
+import com.yelinaung.karrency.app.ui.HomeActivity;
 import com.yelinaung.karrency.app.util.SharePrefUtils;
 
 /**
@@ -38,6 +39,8 @@ public class ConfigureCurrency extends Activity {
 
   int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
+  SharePrefUtils sharePrefUtils;
+
   @InjectView(R.id.ok_btn) Button okayButton;
   @InjectView(R.id.currency_radio_group) RadioGroup currencyRadioGroup;
 
@@ -46,30 +49,7 @@ public class ConfigureCurrency extends Activity {
     setResult(RESULT_CANCELED);
     setContentView(R.layout.choose_currency);
     ButterKnife.inject(this);
-
-    okayButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        final Context context = ConfigureCurrency.this;
-        int id = currencyRadioGroup.getCheckedRadioButtonId();
-        View radioButton = currencyRadioGroup.findViewById(id);
-        int idx = currencyRadioGroup.indexOfChild(radioButton);
-        Log.i("configure", "selected value " + idx);
-
-        SharePrefUtils.getInstance(ConfigureCurrency.this).saveSelectedValue(idx);
-
-        Log.i("saved value",
-            SharePrefUtils.getInstance(ConfigureCurrency.this).getSavedValue() + "");
-
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-
-        ExchangeRateWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
-
-        Intent resultValue = new Intent();
-        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-        setResult(RESULT_OK, resultValue);
-        finish();
-      }
-    });
+    sharePrefUtils = SharePrefUtils.getInstance(this);
 
     Intent intent = getIntent();
     Bundle extras = intent.getExtras();
@@ -77,6 +57,55 @@ public class ConfigureCurrency extends Activity {
       mAppWidgetId =
           extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
     }
+
+    okayButton.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        final Context context = ConfigureCurrency.this;
+        int id = currencyRadioGroup.getCheckedRadioButtonId();
+        View radioButton = currencyRadioGroup.findViewById(id);
+        int idx = currencyRadioGroup.indexOfChild(radioButton);
+
+        sharePrefUtils.saveSelectedValue(idx);
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+        RemoteViews views =
+            new RemoteViews(context.getPackageName(), R.layout.exchange_rate_widget);
+
+        if (idx == 0) {
+          views.setTextViewText(R.id.label_currency, "1 USD");
+          views.setTextViewText(R.id.currency_value, sharePrefUtils.getUSD() + " MMK");
+        } else if (idx == 1) {
+          views.setTextViewText(R.id.label_currency, "1 SGD");
+          views.setTextViewText(R.id.currency_value, sharePrefUtils.getSGD() + " MMK");
+        } else if (idx == 2) {
+          views.setTextViewText(R.id.label_currency, "1 EUR");
+          views.setTextViewText(R.id.currency_value, sharePrefUtils.getEUR() + " MMK");
+        } else if (idx == 3) {
+          views.setTextViewText(R.id.label_currency, "1 MYR");
+          views.setTextViewText(R.id.currency_value, sharePrefUtils.getMYR() + " MMK");
+        } else if (idx == 4) {
+          views.setTextViewText(R.id.label_currency, "1 GBP");
+          views.setTextViewText(R.id.currency_value, sharePrefUtils.getGBP() + " MMK");
+        } else if (idx == 5) {
+          views.setTextViewText(R.id.label_currency, "1 THB");
+          views.setTextViewText(R.id.currency_value, sharePrefUtils.getTHB() + " MMK");
+        }
+
+        Intent intent = new Intent(context, HomeActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.widget_configure, pendingIntent);
+
+        ExchangeRateWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+
+        appWidgetManager.updateAppWidget(mAppWidgetId, views);
+
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+        setResult(RESULT_OK, resultValue);
+        finish();
+      }
+    });
 
     // If they gave us an intent without the widget id, just bail.
     if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
